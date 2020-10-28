5a7159b5617e5c9e03a6bf06816cf20b2b10ad14
Stefan Rajinovic <rajinovic@student.tugraz.at>
2020-10-28 18:09:28 +0100

    2.10. CALCULATION

---
+++ isr.py
@@ -0,0 +1,92 @@
+import sys
+import os
+import math
+
+input_file = sys.argv
+
+
+# b
+def count_freq(words_list, word):
+    counter = 0
+    for sec_w in words_list:
+        if word == sec_w:
+            counter += 1
+    return counter
+
+
+def count_word_all_doc(word):
+    cnt = 0
+    for arr in [d1_sorted, d2_sorted, d3_sorted, d4_sorted, d5_sorted]:
+        cnt += count_freq(arr, word)
+
+    print("Word " + ">>"+ word +"<<" + " in all documents " + str(cnt))
+    return cnt
+
+
+def count_word_in_doc(word):
+    words_in_doc = 0
+    for doc in [d1_sorted, d2_sorted, d3_sorted, d4_sorted, d5_sorted]:
+        if word in doc:
+            words_in_doc += 1
+    print("Word " + ">>"+ word+ "<<" + "repeated in " + str(words_in_doc) + " documents")
+    return words_in_doc
+
+d1 = "information retrieval introduction information retrieval model searching browsing search brows"
+
+d2 = "information retrieval data retrieval retrieval mechanism information science user interface ranking relevance"
+
+d3 = "data information knowledge information retrieval information science relevance feedback information retrieval history"
+
+d4 = "search service hypertext data retrieval model information need"
+
+d5 = "search service search engine meta search catalogue search service portal"
+
+d1_sorted = sorted(d1.split(" "))
+d2_sorted = sorted(d2.split(" "))
+d3_sorted = sorted(d3.split(" "))
+d4_sorted = sorted(d4.split(" "))
+d5_sorted = sorted(d5.split(" "))
+
+# a
+print(d1_sorted)
+print(d2_sorted)
+print(d3_sorted)
+print(d4_sorted)
+print(d5_sorted)
+
+# b Fi
+print(count_freq(d1_sorted, "information"))
+print(count_freq(d3_sorted, "data"))
+print(count_freq(d5_sorted, "search"))
+
+
+count_word_all_doc("information")
+count_word_all_doc("hypertext")
+
+
+count_word_in_doc("information")
+count_word_in_doc("hypertext")
+
+
+print("information" in d1_sorted)
+print("information" in d2_sorted)
+print("information" in d3_sorted)
+print("information" in d4_sorted)
+print("information" in d5_sorted)
+
+N = count_word_all_doc("information")
+N_hypertext = count_word_all_doc("hypertext")
+n_i = count_word_in_doc("information")
+n_i_hypertext = count_word_in_doc("hypertext")
+
+id_fi = math.log(5/n_i, 2)
+id_fi_hypertext = math.log(5/n_i_hypertext, 2)
+print("information inverse frequency " + str(id_fi))
+print("hypertext inverse frequency " + str(id_fi_hypertext))
+
+print("information inverse frequency smooth " + str(math.log(1 + 5/n_i, 2)))
+print("hypertext inverse frequency smooth " + str(math.log(1 + 5/n_i_hypertext, 2)))
+
+print("information inverse frequency max " + str(math.log(1 + n_i/n_i, 2)))
+print("hypertext inverse frequency max " + str(math.log(1 + n_i/n_i_hypertext, 2)))
+
