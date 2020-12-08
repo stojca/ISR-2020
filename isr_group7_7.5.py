261ddcf2497a04b24f72ebc44b44ddbeae7e4497
Aleksandar Stojicic <aleksandar.stojicic@bearingpoint.com>
2020-12-08 18:31:03 +0100

    7.6 finished

--- isr_group7_7.5.py
+++ isr_group7_7.5.py
@@ -9,80 +9,53 @@ ingoing = {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [1,2,3], 8:[4,5,6], 9:[4
 
 auths = [1] * 12
 hubs = [1] * 12
-#total number of pages on the Web Graph
-#PR(pi) = 1/T
+
 
 q=0.15
 T=12
 
-
-
-
-##b1   Iteration 1###
-initial_value = 1 / T
-print("initial value: " + str(initial_value))
-
-
-sum_pr = 0
-sum_pr_array = []
-for outgoing_element in outgoing:
-    sum = 0
-    if len(ingoing[outgoing_element]) != 0:
-        for ingoing_element in ingoing[outgoing_element]:
-            sum += initial_value / len(outgoing[ingoing_element])
-
-    PR_a_normalized = q / T + (1 - q) * sum
-    print("document " + str(outgoing_element))
-    print("not normalized value " + str(PR_a_normalized))
-    sum_pr += PR_a_normalized
-    sum_pr_array.append(PR_a_normalized)
-
-#normalized
-c = 1.0 / sum_pr
-sum_normalized = 0
 sum_pr_array_normalized = []
-for index, pr_a in enumerate(sum_pr_array):
+for iteration in range(3):
+    print("###########################################################################################")
+    print("iteration " + str(iteration + 1))
+
+    initial_value = 1 / T
+    print("initial value: " + str(initial_value))
 
 
-    PR_a_normalized = pr_a * float(c)
-    sum_pr_array_normalized.append(PR_a_normalized)
-    print("document " + str(index + 1))
-    print("normalized: ")
-    print(PR_a_normalized)
-    sum_normalized += PR_a_normalized
+    sum_pr = 0
+    sum_pr_array = []
 
-print("sum of all normalized after first iteration " + str(sum_normalized))
+    for outgoing_element in outgoing:
+        sum = 0
+        if len(ingoing[outgoing_element]) != 0:
+            for ingoing_element in ingoing[outgoing_element]:
+                if(iteration >= 1):
+                    #print("index: " + str(outgoing_element - 1))
+                    sum += sum_pr_array_normalized[ingoing_element - 1] / len(outgoing[ingoing_element])
+                else:
+                    sum += initial_value / len(outgoing[ingoing_element])
 
-###Iteration2##########################
-print("iteration 2")
-sum_pr = 0
-sum_pr_array = []
-index = 0
-for outgoing_element in outgoing:
-    sum = 0
-    if len(ingoing[outgoing_element]) != 0:
-        for ingoing_element in ingoing[outgoing_element]:
-            sum += sum_pr_array_normalized[index] / len(outgoing[ingoing_element])
+        PR_a_normalized = q / T + (1 - q) * sum
+        print("document " + str(outgoing_element))
+        print("not normalized value " + str(PR_a_normalized))
+        sum_pr += PR_a_normalized
+        sum_pr_array.append(PR_a_normalized)
 
-    PR_a_normalized = q / T + (1 - q) * sum
-    print("document " + str(outgoing_element))
-    print("not normalized value " + str(PR_a_normalized))
-    sum_pr += PR_a_normalized
-    sum_pr_array.append(PR_a_normalized)
-    index += 1
+    #normalized
+    c = 1.0 / sum_pr
+    sum_normalized = 0
 
-#normalized
-c = 1.0 / sum_pr
-sum_normalized = 0
-sum_pr_array_normalized = []
-for index, pr_a in enumerate(sum_pr_array):
+    sum_pr_array_normalized.clear()
+    for index, pr_a in enumerate(sum_pr_array):
+        PR_a_normalized = pr_a * float(c)
+        sum_pr_array_normalized.append(PR_a_normalized)
+        print("document " + str(index + 1))
+        print("normalized: ")
+        print(PR_a_normalized)
+        sum_normalized += PR_a_normalized
+
+    print(sum_pr_array_normalized)
+    print("sum of all normalized after " + str(iteration + 1)  +  " iteration " + str(sum_normalized))
 
 
-    PR_a_normalized = pr_a * float(c)
-    sum_pr_array_normalized.append(PR_a_normalized)
-    print("document " + str(index + 1))
-    print("normalized: ")
-    print(PR_a_normalized)
-    sum_normalized += PR_a_normalized
-
-print("sum of all normalized after second iteration " + str(sum_normalized))
